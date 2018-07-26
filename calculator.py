d1f34512bd76ece660016bc4ef86ef69a38953c7
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-07-25 21:30:19 -0700

    got the add, subtract, divide, and multiply functions to work. Now need to get the text page up

--- calculator.py
+++ calculator.py
@@ -40,16 +40,43 @@ To submit your homework:
 
 
 """
-
+import traceback
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
 
     # TODO: Fill sum with the correct value, based on the
     # args provided.
-    sum = "0"
-
+    sum = str(args[0] + args[1])
     return sum
+
+
+def subtract(*args):
+    """ Returns a STRING with the differenceof the arguments """
+
+    # TODO: Fill sum with the correct value, based on the
+    # args provided.
+    difference = str(args[0] - args[1])
+    return difference
+
+
+def divide(*args):
+    """ Returns a STRING with the quotient of the arguments """
+
+    # TODO: Fill sum with the correct value, based on the
+    # args provided.
+    quotient = str(args[0] / args[1])
+    return quotient
+
+
+def multiply(*args):
+    """ Returns a STRING with the multiplier  of the arguments """
+
+    # TODO: Fill sum with the correct value, based on the
+    # args provided.
+    multiplier = str(args[0] / args[1])
+    return multiplier
+
 
 # TODO: Add functions for handling more arithmetic operations.
 
@@ -58,27 +85,76 @@ def resolve_path(path):
     Should return two values: a callable and an iterable of
     arguments.
     """
+    # Given that path is something like: 
+    # 'add/3/3'
+    # 'divide/10/5/'
+    # '/multiply/8/6/'
+
+
+
+    path = path.strip('/').split('/')   #  now ive got something like                                     #  ["add", "3","4"]
+    func_name = path.pop(0)  # "add" and ["3", "4"]
+    args = [int(arg) for arg in path]  # args is now ["3","4"]
+    funcs = {
+            'add': add,
+            'subtract': subtract,
+            'multiply': multiply,
+            'divide': divide,
+            }
+    func = funcs[func_name]
+    # try:
+    #     func = funcs[func_name]
+    # except KeyError:
+    #     raise NameError 
 
     # TODO: Provide correct values for func and args. The
     # examples provide the correct *syntax*, but you should
     # determine the actual values of func and args using the
     # path.
-    func = add
-    args = ['25', '32']
+    # func = add
+    # args = ['25', '32']
 
     return func, args
 
+    # has to recieve the environ and then receive the start
+    # response function. it takes these requests and then 
+    # figures out what response. 
 def application(environ, start_response):
     # TODO: Your application code from the book database
     # work here as well! Remember that your application must
     # invoke start_response(status, headers) and also return
     # the body of the response in BYTE encoding.
-    #
+
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None) #requested path 
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path) # returns a func and args for 
+        body = func(*args)              # the function
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]  # returns the body as bites
+
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+
+
+# WSGI is split between a server and an application the server
+# is the part of the request that breaks it down and figures out 
+# what those requests are. we are using a built in server. 
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
+
